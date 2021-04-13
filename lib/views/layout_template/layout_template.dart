995d18b366f63333cb6ae3bdac9efc9f97842745
Maulik-Khandelwal <kmaulik07@gmail.com>
2021-04-13 23:04:43 +0530

    Background changed.

--- lib/views/layout_template/layout_template.dart
+++ lib/views/layout_template/layout_template.dart
@@ -59,15 +59,15 @@ class PictureWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenTypeLayout(
       mobile: Image(
-        image: new AssetImage("assets/BGM1.jpg"),
+        image: new AssetImage("assets/123.jpg"),
         fit: BoxFit.cover,
-        color: Colors.black87.withOpacity(0.9),
+        color: Colors.black87.withOpacity(0.75),
         colorBlendMode: BlendMode.darken,
       ),
       desktop: Image(
-        image: new AssetImage("assets/BGM1.jpg"),
+        image: new AssetImage("assets/123.jpg"),
         fit: BoxFit.cover,
-        color: Colors.black87.withOpacity(0.8),
+        color: Colors.black87.withOpacity(0.75),
         colorBlendMode: BlendMode.darken,
       ),
     );
